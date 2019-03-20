4928f822cd248ca6f2aa60656c009203153228e7
neboyarsky <1833Yt,jzhcrb>
2019-03-20 03:43:00 +0300

    finished objectville

---
+++ src/Chapter2Objectville/GuessGamePkg/GameLauncher.java
@@ -0,0 +1,8 @@
+package Chapter2Objectville.GuessGamePkg;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
