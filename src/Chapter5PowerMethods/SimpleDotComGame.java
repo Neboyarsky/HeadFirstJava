07fed90b6ea902b285e67614ae6f0b2975f207f6
neboyarsky <1833Yt,jzhcrb>
2019-04-08 15:57:58 +0300

    Building SimpleDotCom game - starting

---
+++ src/Chapter5PowerMethods/SimpleDotComGame.java
@@ -0,0 +1,22 @@
+package Chapter5PowerMethods;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+        while (isAlive == true) {
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("It took you " + numOfGuesses + " guesses to sink the DotCom");
+            } // close if
+        } // close while
+    }
+}
